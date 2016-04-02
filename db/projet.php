35ff600a1a94437e777b58d5f5e122e7eb1acbf7
emileNetter <emile@netter.fr>
2016-04-02 15:34:35 +0200

    gérer candidatures

--- db/projet.php
+++ db/projet.php
@@ -83,11 +83,14 @@ function projet_affiche_infos($id)
 
 function groupe_sauve($post)
 {
-    $tmp= connect_db();
+        $tmp= connect_db();
         $post[sujet]=  addslashes($post[sujet]);
         $post[description] = addslashes($post[description]);       
-        $sql = "INSERT INTO groupes (id,sujet,description, id_responsable,id_eleve_participant) VALUES ('','$post[sujet]','$post[description]','$_SESSION[id]','$_SESSION[id]')";
-        $result = mysqli_query($tmp,$sql);
+        $sql = "INSERT INTO groupes (id,sujet,description, id_responsable,id_eleve_participant) VALUES ('','$post[sujet]','$post[description]','$_SESSION[id]','$_SESSION[id]')"; 
+        mysqli_query($tmp,$sql);
+        $lastId=mysqli_insert_id($tmp);
+        $sql1 = "INSERT INTO groupes_utilisateurs (id_groupe,id_utilisateur,role,statut,message) VALUES ('$lastId','$_SESSION[id]','administrateur','terminé','') ";
+        mysqli_query($tmp,$sql1);
         $message = mysqli_error($tmp);
         $_SESSION['message']=$message;
 
@@ -102,10 +105,18 @@ function groupe_supprime($id,$id_utilisateur)
     
      if ($row['id_responsable']==$id_utilisateur)
      {
-        $sql = "DELETE FROM groupes WHERE id= $row[id]";
+        $sql1="DELETE FROM groupes_utilisateurs WHERE id_groupe=$id";
+        mysqli_query($tmp,$sql1);
+        $sql = "DELETE FROM groupes WHERE id=$id";
         $result = mysqli_query($tmp,$sql);
      }
-    return $result;
+    if (!$result) {
+        die('Impossible d\'exécuter la requête :' . mysql_error());
+    }
+    else
+    {
+        return $result;
+    }
 }
 function my_group_search()
 {
@@ -118,4 +129,71 @@ function group_search_all_but_me()
     $tmp= connect_db();
     $req = mysqli_query($tmp,"SELECT groupes.*, utilisateurs.nom, utilisateurs.prenom FROM groupes,utilisateurs WHERE groupes.id_responsable = utilisateurs.id AND groupes.id_eleve_participant !=".$_SESSION['id']);
     return $req;
+}
+
+function recherche_groupe_rejoindre($id)
+{
+    $tmp= connect_db();
+    $req = mysqli_query($tmp,"SELECT * FROM groupes where id=".$id);
+    $row = $req->fetch_assoc();
+    return $row;
+}
+
+function rejoindre_groupe($post)
+{
+    $tmp = connect_db();
+    $sql = "INSERT INTO groupes_utilisateurs (id_groupe,id_utilisateur,role,statut,message) VALUES ('$post[id_groupe]','$_SESSION[id]','membre','en attente','$post[message]')";
+    $req=mysqli_query($tmp,$sql);
+    return $req;
+}
+
+function chercher_candidature_en_attente()
+{
+    $tmp = connect_db();
+    $sql="SELECT groupes_utilisateurs.*,utilisateurs.nom as nom_utilisateur, utilisateurs.prenom, groupes.sujet FROM groupes_utilisateurs,groupes,utilisateurs WHERE groupes_utilisateurs.id_groupe IN ( SELECT id_groupe from groupes_utilisateurs WHERE role ='administrateur' AND statut='terminé' AND id_utilisateur =".$_SESSION['id'].") AND role ='membre' AND statut='en attente' AND groupes_utilisateurs.id_utilisateur = utilisateurs.id AND groupes.id = groupes_utilisateurs.id_groupe";
+    $req=  mysqli_query($tmp,$sql);
+    return $req;
+}
+
+function modifier_statut_candidature($id,$statut,$id_utilisateur)
+{
+    if (est_administrateur(cherche_id_groupe_demande($id), $id_utilisateur))
+    {
+        $tmp = connect_db();
+        $sql="UPDATE groupes_utilisateurs SET statut= '$statut' WHERE id='$id'";
+        $result=mysqli_query($tmp, $sql);
+        if (!$result) {
+        die('Impossible d\'exécuter la requête :' . mysql_error());
+        }
+    }
+    else
+    {
+        $_SESSION['message']="Vous n'êtes pas administrateur, impossible de modifier la demande de candidature.";
+    }
+    
+}
+
+function est_administrateur($id_groupe,$id_utilisateur)
+{
+   $tmp = connect_db();
+   $sql = "SELECT * FROM groupes WHERE id = $id_groupe";
+   $result = mysqli_query($tmp,$sql);
+   $row = $result->fetch_assoc();
+   if ($row['id_responsable']==$id_utilisateur)
+     {
+        return true;
+     }
+   else
+   {
+       return false;
+   }
+}
+
+function cherche_id_groupe_demande ($id_candidature)
+{
+    $tmp = connect_db();
+    $sql="SELECT id_groupe FROM groupes_utilisateurs WHERE id=$id_candidature";
+    $result = mysqli_query($tmp,$sql);
+    $row = $result->fetch_assoc();
+    return $row['id_groupe'];
 }
